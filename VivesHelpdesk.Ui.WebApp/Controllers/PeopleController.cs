d0892a07f87655cb10c95176ed1b60054e19733b
Ferretkint <92437473+Ferretkint@users.noreply.github.com>
2022-12-07 09:00:40 +0100

    Initial commit

---
+++ VivesHelpdesk.Ui.WebApp/Controllers/PeopleController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VivesHelpdesk.Ui.WebApp.Data;
+using VivesHelpdesk.Ui.WebApp.Models;
+
+namespace VivesHelpdesk.Ui.WebApp.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private readonly VivesHelpdeskDbContext _dbContext;
+
+        public PeopleController(VivesHelpdeskDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var people = _dbContext.People
+                .ToList();
+            return View(people);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromForm]Person person)
+        {
+            //Validate
+            if (!ModelState.IsValid)
+            {
+                return View(person);
+            }
+            
+            _dbContext.People.Add(person);
+
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit([FromRoute]int id)
+        {
+            var person = _dbContext.People
+                .SingleOrDefault(t => t.Id == id);
+
+            if(person is null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(person);
+        }
+
+        [HttpPost]
+        public IActionResult Edit([FromRoute]int id, [FromForm]Person person)
+        {
+            //Validate
+            if (!ModelState.IsValid)
+            {
+                return View(person);
+            }
+
+            var dbPerson = _dbContext.People
+                .SingleOrDefault(t => t.Id == id);
+
+            if(dbPerson is null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            dbPerson.FirstName = person.FirstName;
+            dbPerson.LastName = person.LastName;
+
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpPost]
+        [Route("[controller]/Delete/{id:int?}")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var person = _dbContext.People
+                .SingleOrDefault(t => t.Id == id);
+
+            if(person is null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            _dbContext.People.Remove(person);
+
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ VivesHelpdesk.Ui.WebApp/Controllers/TicketController.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VivesHelpdesk.Ui.WebApp.Data;
+using VivesHelpdesk.Ui.WebApp.Models;
+
+namespace VivesHelpdesk.Ui.WebApp.Controllers
+{
+    public class TicketController : Controller
+    {
+        private readonly VivesHelpdeskDbContext _dbContext;
+
+        public TicketController(VivesHelpdeskDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index(int? assignedToId)
+        {
+            //var tickets = _dbContext.Tickets
+            //    .Where(t => !assignedToId.HasValue || t.AssignedToId == assignedToId)
+            //    .Include(t => t.AssignedTo)
+            //    .ToList();
+
+            var query = _dbContext.Tickets.AsQueryable();
+
+            if (assignedToId.HasValue)
+            {
+                query = query.Where(t => t.AssignedToId == assignedToId);
+            }
+
+            var tickets = query
+                .Include(t => t.AssignedTo)
+                .ToList();
+
+            return View(tickets);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromForm]Ticket ticket)
+        {
+            //Validate
+            if (!ModelState.IsValid)
+            {
+                return View(ticket);
+            }
+
+            //Execute
+            ticket.CreatedDate = DateTime.UtcNow;
+
+            _dbContext.Tickets.Add(ticket);
+
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit([FromRoute]int id)
+        {
+            var ticket = _dbContext.Tickets
+                .SingleOrDefault(t => t.Id == id);
+
+            if(ticket is null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(ticket);
+        }
+
+        [HttpPost]
+        public IActionResult Edit([FromRoute]int id, [FromForm]Ticket ticket)
+        {
+            //Validate
+            if (!ModelState.IsValid)
+            {
+                return View(ticket);
+            }
+
+            var dbTicket = _dbContext.Tickets
+                .SingleOrDefault(t => t.Id == id);
+
+            if(dbTicket is null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            dbTicket.Title = ticket.Title;
+            dbTicket.Description = ticket.Description;
+            dbTicket.Author = ticket.Author;
+
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var ticket = _dbContext.Tickets
+                .SingleOrDefault(t => t.Id == id);
+
+            return View(ticket);
+        }
+
+        [HttpPost]
+        [Route("[controller]/Delete/{id:int?}")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var ticket = _dbContext.Tickets
+                .SingleOrDefault(t => t.Id == id);
+
+            if(ticket is null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            _dbContext.Tickets.Remove(ticket);
+
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
